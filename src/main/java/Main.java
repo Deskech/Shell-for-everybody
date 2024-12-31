4664dd3a0c2ca3434b280d7a9b7390fa2ce13527
Deskech <jose.manuellinan@hotmail.com>
2024-12-31 13:29:11 -0500

    REPL

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        do {
 
-        System.out.print("$ ");
+            System.out.print("$ ");
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            System.out.println(input + ": command not found");
 
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-
-        System.out.println(input + ": command not found");
+        } while (true);
     }
 }
