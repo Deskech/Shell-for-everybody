be88ee542fb2bb47aaeb7e4c922d9b442b412347
Deskech <jose.manuellinan@hotmail.com>
2024-12-31 13:48:21 -0500

    exit command test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        boolean exit = false;
         do {
 
             System.out.print("$ ");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
-            System.out.println(input + ": command not found");
-
-        } while (true);
+            if (input.equals("exit 0")) {
+                exit = true;
+            } else {
+                System.out.println(input + ": command not found");
+            }
+        } while (!exit);
     }
 }
