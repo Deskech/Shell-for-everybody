e2c1f9c10d93ff4d414071b297449b34fed5c45a
Deskech <jose.manuellinan@hotmail.com>
2024-12-31 14:29:34 -0500

    echo command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,25 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         boolean exit = false;
+
         do {
 
             System.out.print("$ ");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
+
             if (input.equals("exit 0")) {
                 exit = true;
-            } else {
+            } else if(input.matches("^echo .+")) {
+               String filteredData = input.replace("echo","");
+               System.out.println(filteredData);
+            }else {
                 System.out.println(input + ": command not found");
             }
+
         } while (!exit);
     }
 }
